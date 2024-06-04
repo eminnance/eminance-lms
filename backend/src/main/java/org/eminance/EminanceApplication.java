50f934593294d24175838728dd539b40656ebd29
DESKTOP-RAJBO8B\bilel-dev <bilel.daikhi@gmail.com>
2024-06-04 09:55:24 +0100

    docker files

---
+++ backend/src/main/java/org/eminance/EminanceApplication.java
@@ -0,0 +1,19 @@
+package org.eminance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+@SpringBootApplication
+public class JavacornerAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavacornerAdminApplication.class, args);
+    }
+
+}
+}
---
+++ backend/src/test/java/org/eminance/JavacornerAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eminance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacornerAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
